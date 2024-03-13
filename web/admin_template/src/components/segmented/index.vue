0b06bf243f519e9e2175f4a5ddc91947e8067c60
Feynman <2901954062@qq.com>
2024-03-13 18:25:53 +0800

    feat: segmented 组件完善

--- web/admin_template/src/components/segmented/index.vue
+++ web/admin_template/src/components/segmented/index.vue
@@ -3,7 +3,7 @@
     <div class="admin_tab" ref="refTab">
       <div class="admin_tab_active_bar"></div>
       <!-- is_active -->
-      <div class="admin_tab_item" v-for="(item,index) in items" :key="index">
+      <div class="admin_tab_item" style="width: 100px;" v-for="(item,index) in items" :key="index" @click="handleTapItem(index)">
         <div class="admin_tab_item_label">
           {{ item.label }}
         </div>
@@ -17,26 +17,38 @@ import { onMounted, ref } from "vue";
 const emit = defineEmits(['update:modelValue','change']);
 
 const refTab = ref<HTMLElement | null>(null);
-const itemWidths = ref<number[]>([]);
+let itemWidths = ref<number[]>([]);
 let currentIndex = ref<number>(0)
 
+// 定义一个接口来表示每个 item 的结构
+interface Item {
+  label: string;
+  value: number;
+}
 const props = defineProps({
   // tab当前数据
   modelValue: Number,
   items: {
-    type: Array,
+    type: Array as ()=>Item[],
     default: ()=>[
       { label: '密码登录', value: 1 },
       { label: '扫码登录', value: 2 }
     ]
   },
 });
+
+const handleTapItem = (index:number)=>{
+  currentIndex.value = index;
+  if(index >= 0 && index < props.items.length){
+    emit('update:modelValue', props.items[index].value)
+  }
+}
 onMounted(() => {
   // 组件挂载后，获取 .admin_tab 元素
   const tabDom = refTab.value
   if(tabDom){
     let itemDom = tabDom.querySelectorAll('.admin_tab_item');
-    let widths = Array.from(items).map((item) => item.clientWidth);
+    let widths = Array.from(itemDom).map((item) => item.clientWidth);
     itemWidths.value = widths;
   }
   console.log("tabDom=====",tabDom)
@@ -49,6 +61,8 @@ onMounted(() => {
       box-sizing: border-box;
       background: #f5f5f5;
       border-radius: 4px;
+      display: flex;
+      white-space: nowrap;
       .admin_tab_item{
         height: 28px;
         line-height: 28px;
